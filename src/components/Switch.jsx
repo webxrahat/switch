b995953330d33bc41f74bf06f481553fdf651543
Md Rahat Islam <islammdrahat308@gmail.com>
2024-12-08 22:09:28 +0600

    Toggle switch case with background dark

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -7,11 +7,21 @@ const Switch = () => {
   setTogglei(!isTogglei);
  };
 
+ //  console.log(isTogglei);
+
  return (
-  <div className="h-screen flex justify-center items-center ">
+  <div
+   className={`h-screen flex justify-center items-center ease-in-out transition-all duration-700 ${
+    isTogglei ? "bg-black" : "bg-white"
+   }`}
+  >
    <div onClick={handleClickToggle} className=" w-5xl">
     <div className="rounded-full bg-slate-300 pr-44 py-5">
-     <div className="p-1 bg-slate-100 rounded-full">
+     <div
+      className={`p-1 bg-slate-100 rounded-full ease-in-out transition-transform duration-700 ${
+       isTogglei ? "translate-x-40" : "translate-x-3"
+      }`}
+     >
       <span className="p-5 block bg-white rounded-full text-3xl cursor-pointer">
        {isTogglei ? "OFF" : "ON"}
       </span>
